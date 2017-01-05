ec2ef5da9a84a8f00d4b508cc162cebbb2906522
Igor Baković <igorbakovic96@gmail.com>
2017-01-05 18:27:45 +0000

    Dodat GUI za ToolBarView i MenuBarView.

--- src/view/MenuBarView.java
+++ src/view/MenuBarView.java
@@ -9,7 +9,9 @@ package view;
 import model.Model;
 import java.util.*;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import controller.MenuBarController;
 
@@ -22,6 +24,77 @@ public class MenuBarView extends JMenuBar{
    
    public MenuBarView(Model model) {
 	   this.model = model;
+	   
+	   JMenu fileMenu = new JMenu("File"); 
+	   
+	   JMenuItem newWorkspaceItem = new JMenuItem("New workspace");
+	   JMenuItem closeItem = new JMenuItem("Close");
+	   JMenuItem closeAllItem = new JMenuItem("Close all");
+	   JMenuItem openItem = new JMenuItem("Open");
+	   JMenuItem saveItem = new JMenuItem("Save");
+	   JMenuItem saveAsItem = new JMenuItem("Save as");
+	   JMenuItem exitItem = new JMenuItem("Exit GeRuDok");
+	   
+	   fileMenu.add(newWorkspaceItem);
+	   fileMenu.addSeparator();
+	   fileMenu.add(closeItem);
+	   fileMenu.add(closeAllItem);
+	   fileMenu.addSeparator();
+	   fileMenu.add(openItem);
+	   fileMenu.addSeparator();
+	   fileMenu.add(saveItem);
+	   fileMenu.add(saveAsItem);
+	   fileMenu.addSeparator();
+	   fileMenu.add(exitItem);
+	   
+	   JMenu editMenu = new JMenu("Edit");
+	   
+	   JMenuItem cutItem = new JMenuItem("Cut");
+	   JMenuItem copyItem = new JMenuItem("Copy");
+	   JMenuItem pasteItem = new JMenuItem("Paste");
+	   JMenuItem undoItem = new JMenuItem("Undo");
+	   JMenuItem redoItem = new JMenuItem("Redu");
+	   JMenuItem deleteItem = new JMenuItem("Delete");
+	   
+	   editMenu.add(cutItem);
+	   editMenu.add(copyItem);
+	   editMenu.add(pasteItem);
+	   editMenu.addSeparator();
+	   editMenu.add(undoItem);
+	   editMenu.add(redoItem);
+	   editMenu.addSeparator();
+	   editMenu.add(deleteItem);
+	   
+	   JMenu windowMenu = new JMenu("Window");
+	   
+	   JMenuItem minimizeItem = new JMenuItem("Minimize");
+	   JMenuItem maximizeItem = new JMenuItem("Maximize");
+	   
+	   JMenu lookAndFeelMenu = new JMenu("Look And Feel");
+	   
+	   JMenuItem LnF1 = new JMenuItem("LnF1");
+	   JMenuItem LnF2 = new JMenuItem("LnF2");
+	   JMenuItem LnF3 = new JMenuItem("LnF3");
+	   
+	   lookAndFeelMenu.add(LnF1);
+	   lookAndFeelMenu.add(LnF2);
+	   lookAndFeelMenu.add(LnF3);
+	   
+	   windowMenu.add(minimizeItem);
+	   windowMenu.add(maximizeItem);
+	   windowMenu.addSeparator();
+	   windowMenu.add(lookAndFeelMenu);
+	   
+	   JMenu aboutMenu = new JMenu("About Us");
+	   
+	   JMenuItem PCRItem = new JMenuItem("About PCR");
+	   
+	   aboutMenu.add(PCRItem);
+	   
+	   this.add(fileMenu);
+	   this.add(editMenu);
+	   this.add(windowMenu);
+	   this.add(aboutMenu);	   
    }
 
 }
--- src/view/ToolBarView.java
+++ src/view/ToolBarView.java
@@ -12,16 +12,20 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.io.File;
 import java.util.*;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 import controller.ToolBarControler;
 
 /** @pdOid 89b60961-0c68-4ad1-8eb4-5a8b8a8dec28 */
-public class ToolBarView extends JPanel {
+public class ToolBarView extends JToolBar {
    /** @pdRoleInfo migr=no name=ToolBarControler assc=association1 mult=1..1 type=Composition */
    public ToolBarControler toolBarControler;
    /** @pdRoleInfo migr=no name=Model assc=association11 mult=1..1 */
@@ -29,8 +33,83 @@ public class ToolBarView extends JPanel {
    
    public ToolBarView(Model model) {
 	   this.model = model;
-	   this.setBackground(Color.RED);
-	   this.add(new JLabel("TOOLBAR"));
-   }
+	   
+	   JButton btnNew = new JButton();
+	   btnNew.setToolTipText("New");
+	   btnNew.setIcon(new ImageIcon("src/res/new_toolbar_icon.jpg"));
+	   add(btnNew);
+	   
+	   addSeparator();
+	   
+	   JButton btnOpen = new JButton();
+	   btnOpen.setToolTipText("Open");
+	   btnOpen.setIcon(new ImageIcon("src/res/open_toolbar_icon.jpg"));
+	   add(btnOpen);
+	   
+	   JButton btnSave = new JButton();
+	   btnSave.setToolTipText("Save");
+	   btnSave.setIcon(new ImageIcon("src/res/save_toolbar_icon.jpg"));
+	   add(btnSave);
+	   
+	   JButton btnSaveAs = new JButton();
+	   btnSaveAs.setToolTipText("Save As");
+	   btnSaveAs.setIcon(new ImageIcon("src/res/save_as_toolbar_icon.jpg"));
+	   add(btnSaveAs);
+	   
+	   addSeparator();
+	   
+	   JButton btnUndo = new JButton();
+	   btnUndo.setToolTipText("Undo");
+	   btnUndo.setIcon(new ImageIcon("src/res/undo_toolbar_icon.jpg"));
+	   add(btnUndo);
 
+	   JButton btnRedo = new JButton();
+	   btnRedo.setToolTipText("Redo");
+	   btnRedo.setIcon(new ImageIcon("src/res/redo_toolbar_icon.jpg"));
+	   add(btnRedo);
+	   
+	   addSeparator();
+	   
+	   JButton btnShare = new JButton();
+	   btnShare.setToolTipText("Share");
+	   btnShare.setIcon(new ImageIcon("src/res/share_toolbar_icon.jpg"));
+	   add(btnShare);
+	   
+	   addSeparator();
+	   
+	   JButton btnCut = new JButton();
+	   btnCut.setToolTipText("Cut");
+	   btnCut.setIcon(new ImageIcon("src/res/cut_toolbar_icon.jpg"));
+	   add(btnCut);
+	   
+	   JButton btnCopy = new JButton();
+	   btnCopy.setToolTipText("Copy");
+	   btnCopy.setIcon(new ImageIcon("src/res/copy_toolbar_icon.jpg"));
+	   add(btnCopy);
+	   
+	   JButton btnPaste = new JButton();
+	   btnPaste.setToolTipText("Paste");
+	   btnPaste.setIcon(new ImageIcon("src/res/paste_toolbar_icon.jpg"));
+	   add(btnPaste);
+	   
+	   JButton btnDelete = new JButton();
+	   btnDelete.setToolTipText("Delete");
+	   btnDelete.setIcon(new ImageIcon("src/res/delete_toolbar_icon.jpg"));
+	   add(btnDelete);
+
+	   addSeparator();
+	   
+	   JButton btnMaximize = new JButton();
+	   btnMaximize.setToolTipText("Maximize window");
+	   btnMaximize.setIcon(new ImageIcon("src/res/maximize_toolbar_icon.jpg"));
+	   add(btnMaximize);
+	   
+	   JButton btnMinimize = new JButton();
+	   btnMinimize.setToolTipText("Minimize window");
+	   btnMinimize.setIcon(new ImageIcon("src/res/minimize_toolbar_icon.jpg"));
+	   add(btnMinimize);
+	   
+	   setBackground(new Color(255,255,204));
+   }
 }
+
