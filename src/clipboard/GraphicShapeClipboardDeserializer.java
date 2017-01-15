4c5bdf4b5d365a2087e48e5f03b3baebe74f9f1f
David Davidović <david@davidovic.io>
2017-01-15 23:09:55 +0000

    Obrisani stari fajlovi

--- src/clipboard/GraphicShapeClipboardDeserializer.java
+++
@@ -1,6 +0,0 @@
-package clipboard;
-import model.elements.GraphicShape;
-
-public interface GraphicShapeClipboardDeserializer {
-	public GraphicShape deserialize(String serializedString) throws SerializationDeserializationException;
-}
--- src/clipboard/GraphicShapeClipboardSerializer.java
+++
@@ -1,6 +0,0 @@
-package clipboard;
-import model.elements.GraphicShape;
-
-public interface GraphicShapeClipboardSerializer {
-	public void serialize(GraphicShape shape, StringBuilder destination);
-}
